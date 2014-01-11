7d6eed119a8caf4dba88e05efe2906ea41ab18d7
Typhome <martin.vooremae@gmail.com>
2014-01-11 22:36:46 +0200

    a - conf

--- a/config.php
+++ a/config.php
@@ -1,12 +1,10 @@
 <?php
-$CONFIG_DB['server'] = "localhost";
-$CONFIG_DB['name'] = "staatus";
-$CONFIG_DB['user'] = "staatus";
-$CONFIG_DB['password'] = '';
-$DB['prefix'] = "s_";
+
+$prefiks = "a_";
+
 $CONFIG_DB['error_report'] = 0;
 
-if($CONFIG_DB['error_report'] == 0) {
+/*if($CONFIG_DB['error_report'] == 0) {
     mysql_connect($CONFIG_DB['server'], $CONFIG_DB['user'], $CONFIG_DB['password']) or die("<h1>ERROR</h1>Veebilehel esines viga ning sellest veast on teatatud administraatorile.");
     mysql_select_db($CONFIG_DB['name']) or die("<h1>ERROR</h1>Veebilehel esines viga ning sellest veast on teatatud administraatorile.");
 } else {
@@ -14,11 +12,14 @@ if($CONFIG_DB['error_report'] == 0) {
     mysql_select_db($CONFIG_DB['name']) or die(mysql_error());
 }
 
-unset($CONFIG_DB['password']);
+unset($CONFIG_DB['password']);*/
 
 function user($uid, $col)
 {
-	$query = mysql_fetch_assoc(mysql_query('SELECT * FROM users WHERE id = "'.$uid.'" LIMIT 1'));
-	return $query[$col];
+	//$query = mysql_fetch_assoc(mysql_query('SELECT * FROM users WHERE id = "'.$uid.'" LIMIT 1'));
+	$query = $con->prepare('SELECT * FROM '.$prefiks.' WHERE id = '.$uid.'');
+	while($row = $query->fetch(PDO::FETCH_ASSOC)) {
+		return $row[$col];
+	}
 }
 ?>
